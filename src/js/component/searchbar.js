b483ab6fca88b495b026f3ebddca1bef51be826f
Keiji Nakagawa <kenaga9@icloud.com>
2019-12-10 03:00:34 +0000

    msg

---
+++ src/js/component/searchbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Context } from "../store/appContext";
+
+export default class Searchbar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchInput: null
+		};
+	}
+	render() {
+		return (
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<>
+							<div className="input-group mb-3">
+								<input
+									onChange={e => this.setState({ searchInput: e.target.value })}
+									type="text"
+									className="form-control"
+									placeholder="Recipient's username"
+									aria-label="Recipient's username"
+									aria-describedby="button-addon2"
+								/>
+								<div className="input-group-append">
+									<button
+										onClick={() => actions.searchMovie(this.state.searchInput)}
+										className="btn btn-outline-secondary"
+										type="button"
+										id="button-addon2">
+										Button
+									</button>
+								</div>
+							</div>
+						</>
+					);
+				}}
+			</Context.Consumer>
+		);
+	}
+}
