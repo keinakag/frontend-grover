f0265ab3448ad67086ff817ed10186b018a50e07
Keiji Nakagawa <kenaga9@icloud.com>
2020-01-17 21:41:24 +0000

    text

--- src/js/views/text.js
+++ src/js/views/text.js
@@ -9,6 +9,12 @@ import Navbar from "../component/navbar";
 import { SearchResult } from "./searchResults";
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBFormInline } from "mdbreact";
 export class Text extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchInput: null
+		};
+	}
 	state = {
 		modal10: false,
 		modal11: false
@@ -23,40 +29,43 @@ export class Text extends React.Component {
 
 	render() {
 		return (
-			<MDBContainer>
-				<MDBBtn color="warning" onClick={this.toggle(10)}>
-					Bottom
-				</MDBBtn>
-				<MDBModal isOpen={this.state.modal10} toggle={this.toggle(10)} frame position="bottom">
-					<MDBModalBody className="text-center">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua.
-						<MDBBtn color="secondary" onClick={this.toggle(10)}>
-							Close
-						</MDBBtn>
-						<MDBBtn color="primary">Save changes</MDBBtn>
-					</MDBModalBody>
-				</MDBModal>
-
-				<MDBBtn color="secondary" onClick={this.toggle(11)}>
-					search
-				</MDBBtn>
-				<MDBModal isOpen={this.state.modal11} toggle={this.toggle(11)} frame position="top">
-					<MDBFormInline className="md-form mr-auto mb-4">
-						<input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-						<MDBBtn color="secondary" onClick={this.toggle(11)}>
-							Close
-							<Link to="/text">
-								<button
-									className="search-button"
-									onClick={() => actions.searchMovie(this.state.searchInput)}>
-									<span className="icon" />
-								</button>
-							</Link>
-						</MDBBtn>
-					</MDBFormInline>
-				</MDBModal>
-			</MDBContainer>
+			<Context.Consumer>
+				{({ store, actions }) => {
+					return (
+						<MDBContainer>
+							<button
+								color="secondary"
+								onClick={this.toggle(11)}
+								type="button"
+								className="btn btn-default btn-circle btn-xl">
+								<i className="fas fa-search" />
+							</button>
+							<MDBModal isOpen={this.state.modal11} toggle={this.toggle(11)} frame position="top">
+								<MDBFormInline className="md-form mr-auto mb-4">
+									<input
+										className="form-control mr-sm-2"
+										type="text"
+										placeholder="Search"
+										aria-label="Search"
+										onChange={e => this.setState({ searchInput: e.target.value })}
+									/>
+									<Link to="/text">
+										<button
+											type="button"
+											className="btn btn-default btn-circle btn-xl"
+											onClick={() => actions.searchMovie(this.state.searchInput)}>
+											<i className="fas fa-search" />
+										</button>
+									</Link>
+									<MDBBtn color="secondary" onClick={this.toggle(11)}>
+										Close
+									</MDBBtn>
+								</MDBFormInline>
+							</MDBModal>
+						</MDBContainer>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
