b2818da3f2366c047a37d6d9d872c68665428daf
Keiji Nakagawa <kenaga9@icloud.com>
2020-01-28 06:02:39 +0000

    update card css

--- src/js/component/cardsPng.js
+++ src/js/component/cardsPng.js
@@ -19,67 +19,76 @@ export default class CardsPng extends React.Component {
 
 				<div className="flex-container">
 					<div className="row justify-content-center">
-						<div className="col-8">
-							<div className="card-deck ">
-								<div
-									className="card png h-100 "
-									style={{ backgroundColor: "#024752", textAlign: "center" }}>
-									<img
-										src={Tvlogo}
-										className="card-img-top  img-fluid smallimg mx-auto pt-4"
-										alt="..."
-										style={{ width: "237px", display: "block" }}
-									/>
-									<div className="card-body ">
-										<h3 className="card-title" style={{ color: "white" }}>
-											TV
-										</h3>
-										<p className="card-text" style={{ color: "grey" }}>
-											<p>Amazon Fire TV</p>
-											<p>Android TV </p>
-											<p>AppleTV </p>
-											<p>Chromecast</p>
-											<p>LG TVs</p>
-											<p>Samsung</p>
-											<p>Roku</p>
-										</p>
-									</div>
+						<div className="col-md-6 col-lg-4 col-xl-3">
+							<div
+								id="png"
+								className="card png h-100 w-75 "
+								style={{ backgroundColor: "#024752", textAlign: "center" }}>
+								<img
+									src={Tvlogo}
+									className="card-img-top  img-fluid smallimg mx-auto pt-4"
+									alt="..."
+									style={{ width: "237px", display: "block" }}
+								/>
+								<div className="card-body ">
+									<h3 className="card-title" style={{ color: "white" }}>
+										TV
+									</h3>
+									<p className="card-text" style={{ color: "grey" }}>
+										<p>Amazon Fire TV</p>
+										<p>Android TV </p>
+										<p>AppleTV </p>
+										<p>Chromecast</p>
+										<p>LG TVs</p>
+										<p>Samsung</p>
+										<p>Roku</p>
+									</p>
 								</div>
-								<div className="card png" style={{ backgroundColor: "#024752", textAlign: "center" }}>
-									<img
-										src={Laptop}
-										className="card-img-top  img-fluid smallimg mx-auto pt-4"
-										alt="..."
-										style={{ width: "237px", display: "block" }}
-									/>
-									<div className="card-body border-0">
-										<h3 className="card-title" style={{ color: "white" }}>
-											Computer
-										</h3>
-										<p className="card-text" style={{ color: "grey" }}>
-											<p>Chrome OS</p>
-											<p>MacOS</p>
-											<p> Windows PC</p>
-										</p>
-									</div>
+							</div>
+						</div>
+						<div className="col-md-6 col-lg-4 col-xl-3">
+							<div
+								id="png"
+								className="card png h-100 w-75"
+								style={{ backgroundColor: "#024752", textAlign: "center" }}>
+								<img
+									src={Laptop}
+									className="card-img-top  img-fluid smallimg mx-auto pt-4"
+									alt="..."
+									style={{ width: "237px", display: "block" }}
+								/>
+								<div className="card-body border-0">
+									<h3 className="card-title" style={{ color: "white" }}>
+										Computer
+									</h3>
+									<p className="card-text" style={{ color: "grey" }}>
+										<p>Chrome OS</p>
+										<p>MacOS</p>
+										<p> Windows PC</p>
+									</p>
 								</div>
-								<div className="card png" style={{ backgroundColor: "#024752", textAlign: "center" }}>
-									<img
-										src={Tablet}
-										className="card-img-top img-fluid smallimg mx-auto pt-4"
-										alt="..."
-										style={{ width: "150px", display: "block" }}
-									/>
-									<div className="card-body">
-										<h3 className="card-title" style={{ color: "white" }}>
-											Mobile & Tablet
-										</h3>
-										<p className="card-text" style={{ color: "grey" }}>
-											<p>Amazon Fire Tablets</p>
-											<p>Android Phones & Tablets</p>
-											<p>iPhone and iPad</p>
-										</p>
-									</div>
+							</div>
+						</div>
+						<div className="col-md-6 col-lg-4 col-xl-3">
+							<div
+								id="png"
+								className="card png h-100 w-75"
+								style={{ backgroundColor: "#024752", textAlign: "center" }}>
+								<img
+									src={Tablet}
+									className="card-img-top img-fluid smallimg mx-auto pt-4"
+									alt="..."
+									style={{ width: "150px", display: "block" }}
+								/>
+								<div className="card-body">
+									<h3 className="card-title" style={{ color: "white" }}>
+										Mobile & Tablet
+									</h3>
+									<p className="card-text" style={{ color: "grey" }}>
+										<p>Amazon Fire Tablets</p>
+										<p>Android Phones & Tablets</p>
+										<p>iPhone and iPad</p>
+									</p>
 								</div>
 							</div>
 						</div>
